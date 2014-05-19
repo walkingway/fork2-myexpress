51b7ccbae42bf8a394e0d8109b243171e2b5b96b
walkingway <walkingway@126.com>
2014-05-20 01:31:25 +0800

    update test/monkey_spec

--- test/monkey_spec.js
+++ test/monkey_spec.js
@@ -81,3 +81,53 @@ describe("Setting req.app",function() {
     });
   });
 });
+
+describe("HTTP redirect:",function() {
+  beforeEach(function() {
+    app = express();
+    app.use("/foo",function(req,res) {
+      res.redirect("/baz"); // default is 302
+    });
+    app.use("/bar",function(req,res) {
+      res.redirect(301,"/baz");
+    });
+  });
+
+  it("redirects with 302 by default",function(done) {
+    request(app).get("/foo")
+      .expect(302)
+      .expect("Location","/baz").end(done);
+  });
+
+  it("redirects with the given status code",function(done) {
+    request(app).get("/bar")
+      .expect(301)
+      .expect("Location","/baz").end(done);
+  });
+
+  it("returns empty body",function(done) {
+    request(app).get("/foo")
+      .expect("")
+      .expect("Content-Length",0).end(done);
+  });
+});
+
+describe("req.res and res.req",function() {
+  beforeEach(function() {
+    app = express();
+  });
+
+  it("makes request and response accessible to each other",function(done) {
+    var _res, _req;
+    app.use(function(req,res) {
+      _res = res; _req = req;
+      res.end("ok");
+    });
+
+    request(app).get("/").end(function() {
+      expect(_res.req).to.equal(_req,"res.req is not request");
+      expect(_req.res).to.equal(_res,"req.res is not response");
+      done();
+    });
+  });
+});
